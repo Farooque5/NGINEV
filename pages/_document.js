46c764cd4e554f9931b756ce8702a9057c9dcd03
Farooque5 <khanmdfarooque559@gmail.com>
2024-11-20 13:38:55 +0530

    head tag set in document

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,31 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <>
+    <Html lang="en" className="h-full">
+      <Head>
+        {/* SEO and Performance Improvements */}
+        <meta charSet="UTF-8" />
+        <meta
+          name="title"
+          content="NGINEV"
+        />
+        <meta
+          name="description"
+          content="NGINEV offers software, hardware, marketing, and printing solutions tailored to empower businesses worldwide."
+        />
+        <meta
+          name="keywords"
+          content="business solutions, software development, hardware support, marketing strategies, printing services, NGINEV"
+        />
+        <meta name="author" content="NGINEV" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+      
       {/* Main Content and Accessibility */}
       <body className="antialiased bg-gray-50">
         <Main />
         <NextScript />
       </body>
-    </>
-      
-      
+    </Html>
   );
 }
